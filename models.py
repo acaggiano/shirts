5ecb50abd4f9c44ac22febb930961ebc317a5a18
Anthony Caggiano <onaiggaca@gmail.com>
2018-03-16 11:54:55 -0400

    implenting last_worn on save

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Shirt(models.Model):
@@ -7,3 +8,7 @@ class Shirt(models.Model):
     image = models.ImageField(upload_to='uploads/shirts/', blank=True)
     last_worn = models.DateField(null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        if self.worn:
+           self.last_worn = datetime.date.today()
+        super().save(*args, **kwargs)
